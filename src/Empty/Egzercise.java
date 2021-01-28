917aeeee395ff5e00f4808749db4951fa5dece3b
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-01-27 22:16:35 -0800

    jan27

---
+++ src/Empty/Egzercise.java
@@ -0,0 +1,11 @@
+package Empty;
+
+import Utility.Fibonacci;
+
+public class Egzercise {
+
+    public static void main(String[] args) {
+
+        System.out.println(Fibonacci.howmanyTerm(12));
+    }
+}
