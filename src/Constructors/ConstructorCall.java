0c1a6ac656a1b55a4efbf3d079d7ce5c9c128158
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-02-10 13:28:53 -0800

    Feb10

--- src/Constructors/ConstructorCall.java
+++ src/Constructors/ConstructorCall.java
@@ -1,5 +1,8 @@
 package Constructors;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ConstructorCall {
     public String firstName,lastName;
     public char gender;
@@ -17,11 +20,50 @@ public class ConstructorCall {
         this.lastName = lastName;
         this.gender = gender;
     }
+
+    @Override
+    public String toString() {
+        return "ConstructorCall{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender=" + gender +
+                ", Age= "+age+
+                ", Job title= "+jobTitle+
+                '}';
+    }
 }
 class ConstructorCallObject{
 
     public static void main(String[] args) {
 
+
+        System.out.println(ConstructorCall.age);
+        System.out.println(ConstructorCall.jobTitle);
+
+        ConstructorCall obj1 = new ConstructorCall("Ali","Jamal",'M');
+        ConstructorCall obj2 = new ConstructorCall("ayse","hayla",'F');
+        ConstructorCall obj3 = new ConstructorCall("Jennifer","kiny",'F');
+        System.out.println(obj1);
+
+        System.out.println("------------------------------------------");
+        ArrayList<String> firstnames =new ArrayList<>(Arrays.asList(obj1.firstName,obj2.firstName,obj3.firstName));
+        ArrayList<ConstructorCall> list =new ArrayList<>(Arrays.asList(obj1,obj2,obj3));
+        System.out.println(firstnames);
+
+        System.out.println("------------------------------------------");
+
+        System.out.println(list);
+
+        System.out.println(obj1.gender =='F');
+
+        ArrayList<ConstructorCall> Female = new ArrayList<>();
+
+        for (ConstructorCall each :list) {
+            if (each.gender=='F'){
+                Female.add(each);
+            }
+        }
+        System.out.println(Female);
 
     }
 }
