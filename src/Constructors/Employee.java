04ed862ff772583c3747d4b344034e75fe8df023
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-02-10 13:03:26 -0800

    Feb10

--- src/Constructors/Employee.java
+++ src/Constructors/Employee.java
@@ -1,37 +1,51 @@
 package Constructors;
 
-import java.text.DecimalFormat;
-
 public class Employee {
+
 
     public String name, jobTitle,ID;
     public double salary;
 
-    public Employee(String name){
-        this.name=name;
+    public static String companyName;
+
+    static {
+        companyName="Tesla";
     }
 
-    public Employee(String name,String jobTitle){
-        this.name=name;
-        this.jobTitle=jobTitle;
+    public Employee() {
     }
 
-    public Employee(String name,String jobTitle,String ID){
-        this(name,jobTitle);
-        this.ID=ID;
+    public Employee(String name) {
+        this.name = name;
     }
 
-    public Employee(String name,String jobTitle,String ID,double salary){
-        this(name,jobTitle,ID);
-        this.salary=salary;
+    public Employee(String name, String jobTitle) {
+        this.name = name;
+        this.jobTitle = jobTitle;
     }
+
+    public Employee(String name, String jobTitle, String ID) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.ID = ID;
+    }
+
+    public Employee(String name, String jobTitle, String ID, double salary) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.ID = ID;
+        this.salary = salary;
+    }
+
 
     @Override
     public String toString() {
-
-        return  "name= " + name + '\n' +
-                "jobTitle= " + jobTitle + '\n' +
-                "ID= " + ID + '\n' +
-                "salary= $" + salary;
+        return "Employee{" +
+                "name= " + name + '\'' +
+                ", jobTitle= " + jobTitle + '\'' +
+                ", ID= " + ID + '\'' +
+                ", salary= " + salary +
+                ", Company Name= "+companyName+
+                '}';
     }
 }
---
+++ src/Constructors/EmployeeObject.java
@@ -0,0 +1,12 @@
+package Constructors;
+
+public class EmployeeObject {
+    public static void main(String[] args) {
+
+        Employee employee1 = new Employee();
+        System.out.println(employee1);
+
+
+
+    }
+}
