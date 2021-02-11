b8f09d2f87b565811375c6e502b80a9d0753a216
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-02-10 18:02:37 -0800

    Feb10

--- src/Encapulation/BankAccount.java
+++ src/Encapulation/BankAccount.java
@@ -1,4 +1,15 @@
 package Encapulation;
-
+///-------------This topic review how to reach type of Private variable-------------
 public class BankAccount {
+
+    public final static String bankName = "Bank Of America";
+
+    public String firstName, lastName;
+
+    private String accountHolder;
+    private int accountNumber;
+    private double balance;
+
+
+
 }
