9b34ce08e0f6fa0f007bc2b0b13e57c51514c31f
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-04-19 18:33:44 -0700

    Apr 19

--- src/ArrayLists/NonDupInt.java
+++ src/ArrayLists/NonDupInt.java
@@ -1,4 +1,23 @@
 package ArrayLists;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class NonDupInt {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> x = new ArrayList<>(Arrays.asList(2, 3, 1, 3, 2, 2, 4, 5, 21, 7, 34, 8, 3, 3, 7, 6, 9, 6));
+        ArrayList<Integer> y = new ArrayList<>();
+        for (int i = 0; i < x.size(); i++) {
+
+            if (!y.contains(x.get(i))){
+                y.add(x.get(i));
+            }
+        }
+
+        System.out.println(y);
+
+
+
+    }
 }
--- src/Empty/Empty1.java
+++ src/Empty/Empty1.java
@@ -8,18 +8,5 @@ public class Empty1 {
 
     public static void main(String[] args )  throws NullPointerException{
 
-
-        ArrayList<Integer> x = new ArrayList<>( Arrays.asList(2,3,1,3,2,2,4,5,21,7,34,8,3,3,7,6,9,6));
-        ArrayList<Integer> y = new ArrayList<>();
-        for (int i = 0; i < x.size(); i++) {
-
-                if (!y.contains(x.get(i))){
-                    y.add(x.get(i));
-                }
-        }
-
-        System.out.println(y);
-
-
     }
 }
