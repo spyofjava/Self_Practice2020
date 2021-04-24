964acc62df09e53dcf5607f6db99c4b23e1c730b
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-04-24 16:09:23 -0700

    Apr 24

--- src/Empty/Empty1.java
+++ src/Empty/Empty1.java
@@ -2,28 +2,10 @@ package Empty;
 
 import java.util.*;
 
-public class Empty1 {
+public class Empty1  {
 
 
-    public static void main(String[] args) throws NullPointerException {
 
 
-        Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
 
-        ArrayList<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i <= number; i++) {
-
-            for (int j = 0; j < number; j++) {
-
-                if (i + j == number) {
-                    list.add(i*j);
-                }
-            }
-        }
-        System.out.println(Collections.max(list));
-
-
-    }
 }
