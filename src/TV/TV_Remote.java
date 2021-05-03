22ef19a8a2ea990b1942d1ca3959ad5912c30ecd
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-05-03 12:37:29 -0700

    May 3

--- src/TV/TV_Remote.java
+++ src/TV/TV_Remote.java
@@ -1,39 +1,85 @@
 package TV;
 
+import java.awt.*;
+import java.util.Collections;
+
 public class TV_Remote {
-    public boolean tvRunning;
+    public boolean Running;
+    public int channelNum = 0;
+    public static final String ANSI_RESET = "\u001B[0m";
+   // public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+   // public static final String ANSI_GREEN = "\u001B[32m";
+   // public static final String ANSI_YELLOW = "\u001B[33m";
+   // public static final String ANSI_BLUE = "\u001B[34m";
+   // public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+  //  public static final String ANSI_WHITE = "\u001B[37m";
 
 
-
-    public boolean On(){
-        if (!tvRunning) {
-            tvRunning = true;
-        }else {
-            System.out.println("Tv is already opened...");
+    public void on() {
+        if (!Running) {
+            Running = true;
+            System.out.println("Tv is "+ANSI_CYAN+"ON"+ANSI_RESET);
+        } else {
+            System.out.println("Tv is already "+ANSI_CYAN+"OPENED"+ANSI_RESET+"...");
         }
-        return tvRunning;
-    }
-    public boolean off(){
 
-        if (tvRunning) {
-            tvRunning = false;
-        }else {
-            System.out.println("Tv is already closed...");
+    }
+
+    public void off() {
+
+        if (Running) {
+            Running = false;
+            System.out.println("Tv is "+ANSI_RED+"OFF"+ANSI_RESET);
+        } else {
+            System.out.println("Tv is already "+ANSI_RED+"CLOSED"+ANSI_RESET+"...");
         }
-        return tvRunning;
     }
 
-    public void channelUp(){
+    public void channelUp() {
 
+        if (Running) {
+            if (channelNum <=98) {
+                channelNum++;
+                System.out.println("Channel "+channelNum);
+            } else {
+                channelNum = 0;
+                System.out.println("Channel "+channelNum);
+            }
 
+        } else {
+            System.out.println("Tv is "+ANSI_RED+"CLOSED"+ANSI_RESET+"...");
+        }
     }
-    public void channelDown(){
 
+
+
+    public void channelDown() {
+        if (Running) {
+            if (channelNum > 0) {
+                channelNum--;
+                System.out.println("Channel "+channelNum);
+            } else {
+                channelNum = 99;
+                System.out.println("Channel "+channelNum);
+            }
+        } else {
+            System.out.println("Tv is "+ANSI_RED+"CLOSED"+ANSI_RESET+"...");
+        }
     }
 
-
-
-
-
+    public void manualChannel(int num) {
+        if (Running) {
+            if (num >= 100 || num < 0) {
+                System.out.println(ANSI_RED+"Invalid channel"+ANSI_RESET+"...");
+            } else {
+                channelNum = num;
+                System.out.println("Channel "+channelNum);
+            }
+        } else {
+            System.out.println("Tv is "+ANSI_RED+"CLOSED"+ANSI_RESET+"...");
+        }
+    }
 
 }
--- src/University/Obj.java
+++ src/University/Obj.java
@@ -7,20 +7,38 @@ public class Obj {
 
     public static void main(String[] args) {
 
-        Academicians a = new Academicians("academicians");
+//        Academicians a = new Academicians("academicians");
 
-        //academicians.LogIn();
+//        //academicians.LogIn();
 
-        a.LogIn();
-        Workers w = new Workers("workers");
-        w.LogIn();
-        Officers o = new Officers("Officers");
-        o.LogIn();
-        o.eat();
-        w.eat();
-        a.eat();
+//        a.LogIn();
+//        Workers w = new Workers("workers");
+//        w.LogIn();
+//        Officers o = new Officers("Officers");
+//        o.LogIn();
+//        o.eat();
+//        w.eat();
+//        a.eat();
         TV_Remote tv = new TV_Remote();
-        System.out.println(tv.tvRunning);
+        tv.on();
+        System.out.println(".......");
+        tv.off();
+        tv.channelUp();
+        tv.on();
+        tv.channelUp();
+        tv.channelDown();
+        tv.channelDown();
+        tv.channelDown();
+        tv.channelUp();
+        tv.channelUp();
+        tv.channelUp();
+        tv.off();
+        tv.off();
+        tv.on();
+        tv.on();
+        tv.manualChannel(222);
+        tv.manualChannel(72);
+
 
     }
 }
