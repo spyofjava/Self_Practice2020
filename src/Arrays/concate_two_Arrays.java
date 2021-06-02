b4c95ab6ab83439cf8be007d4d1fdec2e672cca0
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-06-01 23:06:58 -0700

    Jun1

--- src/Arrays/concate_two_Arrays.java
+++ src/Arrays/concate_two_Arrays.java
@@ -6,7 +6,7 @@ public class concate_two_Arrays {
     public static void main(String[] args) {
 
         int[] list1 = {1, 2, 3, 4, 5};
-        int[] list2 = {55, 4, 5, 6, 7};
+        int[] list2 = {55, 4, 512, 6, 7};
         int[] concated_list = new int[list1.length + list2.length];
 
         for (int i = 0; i < list1.length; i++) {
