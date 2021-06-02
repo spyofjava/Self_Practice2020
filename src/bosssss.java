707a8ef0b9b1ef0d0bc38a1a25abb0486e0e6f82
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-06-01 23:05:28 -0700

    Jun1

--- src/bosssss.java
+++ src/bosssss.java
@@ -1,22 +1,28 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class bosssss {
 
     public static void main(String[] args) {
 
-        int [ ] list1 = {1,2,3,4,5};
-        int [ ] list2 = {3,4,5,6,7};
 
-        int[] cl = new int[list1.length+ list2.length];
-        for (int i = list1.length; i < list1.length+ list2.length; i++) {
-           cl[i] = list2[i];
-       }
-
-
-        System.out.println(Arrays.toString(cl));
+        /////  I N C E L E ///////
 
     }
 
+    public void printPermutation(char[] ch) {
+        for (String s : permutation(ch)) {
+        System.out.println(Arrays.toString(s.toCharArray()));
+        }
+    }
+
+    public Set<String> permutation(char[] ch) {
+        String str = Arrays.toString(ch).replace(", ", "").replace("[", "").replace("]", "");
+        Set<String> set = new LinkedHashSet<>();
+        if (str.length() == 1) { set.add(str);
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                String a3 = str.substring(0, i) + str.substring(i + 1); char[] ch2 = a3.toCharArray();
+                for (String permutation : permutation(ch2)) {
+                    set.add(str.charAt(i) + permutation); } } } return set;
+    }
 }
