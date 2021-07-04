887ff662ae9a260e4127b260756592636107396e
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-07-04 09:55:49 -0700

    Jul4

---
+++ src/Important_Codes/Palindrome.java
@@ -0,0 +1,23 @@
+package Important_Codes;
+
+import java.awt.*;
+import java.util.Collection;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String str = "boby";
+        String result = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            result += str.charAt(i);
+        }
+        if (result.equals(str)){
+            System.out.print(Color.getColor(str).getRed());
+            System.out.println("   is a Palindrome...");
+        }else {
+            System.out.print(str);
+            System.out.print("   not a Palindrome...");
+        }
+
+
+    }
+}
